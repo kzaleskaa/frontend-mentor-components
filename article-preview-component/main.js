afa63a64631c375b502f5147707247cee8ab48e3
Kasia Zaleska <kzaleska416@gmail.com>
2021-10-18 00:41:07 +0200

    change js - window.onclick

--- article-preview-component/main.js
+++ article-preview-component/main.js
@@ -1,7 +1,17 @@
-const share=document.querySelector('.article__btn-share');
-const socials=document.querySelector('.article__socials');
+const share = document.querySelector(".article__btn-share");
+const socials = document.querySelector(".article__socials");
+const text = document.querySelector(".article__share-text");
 
-share.addEventListener("click", function(){
+window.onclick = function (event) {
+  if (event.target == share) {
     share.classList.toggle("open");
-    socials.classList.toggle('hidden');
-});
+    socials.classList.toggle("hidden");
+  } else if (
+    event.target != share &&
+    event.target != socials &&
+    share.classList.contains("open")
+  ) {
+    share.classList.remove("open");
+    socials.classList.add("hidden");
+  }
+};
